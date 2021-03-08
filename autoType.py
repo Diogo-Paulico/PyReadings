c5a4d6b9318f11cedb2be9347a394c1b2c4a5f54
Diogo Paulico <d.paulico@campus.fct.unl.pt>
2021-03-08 18:13:51 +0000

    Added language support for pt and en

--- autoType.py
+++ autoType.py
@@ -1,8 +1,9 @@
 import webbrowser as wb
-from time import sleep
+import time
 from pyautogui import press, write as typeT, keyDown, keyUp
 import defineFunctions as fun
 from os import system, name
+
 
 def start():
     clear()
@@ -15,35 +16,28 @@ def start():
 
 
 def noPlacesAddedMenu():
-    choice = input("""
-                      A: Adicionar Local de Consumo
-                      Q: Exit
-
-                      Please enter your choice: """).lower()
+    choice = input(fun.getString("noplacesmenu") + fun.getString("enterchoice")).lower() #lang["en"]["noplacesmenu"] + lang["en"]["enterchoice"]
 
     if choice =="a":
         register()
         menu()
+    elif choice == "t":
+        clear()
+        fun.toggleLanguage()
+        noPlacesAddedMenu()
     elif choice=="q":
         return
     else:
-        print("You must select one of the available options")
-        print("Please try again")
-        sleep(3)
+        print(fun.getString("selectavailable"))
+        print(fun.getString("tryagain"))
+        time.sleep(3)
         clear()
         noPlacesAddedMenu()
 
 
 
 def menu():
-    choice = input("""
-            A : Adicionar Local de Consumo
-            R : Apagar Local de Consumo
-            E : Escolher Local de Consumo
-            L : Listar Locais de Consumo
-            Q : Exit
-
-            Please enter your choice: """).lower()
+    choice = input(fun.getString("menu") + fun.getString("enterchoice")).lower()
 
     clear()
     if choice =="r":
@@ -59,25 +53,23 @@ def menu():
     elif choice=="l":
         placeDisplay()
         menu()
+    elif choice == "t":
+        clear()
+        fun.toggleLanguage()
+        menu()
     elif choice=="q":
         exitS()
     else:
-        print("You must select one of the available options")
-        print("Please try again")
-        sleep(3)
+        print(fun.getString("selectavailable"))
+        print(fun.getString("tryagain"))
+        time.sleep(3)
         clear()
         menu()
 
 
 def pickedMenu():
     #clear()
-    choice = input("""
-                      V : Ver Última Contagem
-                      C : Comunicar Contagem
-                      L : Voltar ao menu anterior
-                      Q : Exit
-
-                      Please enter your choice: """).lower()
+    choice = input(fun.getString("pickedmenu") + fun.getString("enterchoice")).lower()
     clear()
     if choice =="v":
         showLastReading()
@@ -88,14 +80,18 @@ def pickedMenu():
     elif choice=="l":
         fun.resetChoosenPlace()
         menu()
+    elif choice == "t":
+        clear()
+        fun.toggleLanguage()
+        pickedMenu()
     elif choice=="q":
         exitS()
     else:
-        print("You must select one of the available options")
-        print("Please try again")
-        sleep(3)
+        print(fun.getString("selectavailable"))
+        print(fun.getString("tryagain"))
+        time.sleep(3)
         clear()
-        menu()
+        pickedMenu()
 
 
 def chooseCurrentPlace():
@@ -107,7 +103,7 @@ def placePicker():
     picked = 0
     counter = placeDisplay()
     while picked not in list(range(1, counter)):
-        picked = int(input("Escolha um elemento da lista usando os seus numeros (1 - {0}): ".format(str(counter - 1))))
+        picked = int(input(fun.getString("placepicker").format(str(counter - 1)))) #placepicker
     placeName = fun.getAllPowerSpotsName()[picked-1]
     return placeName
     
@@ -124,13 +120,12 @@ def placeDisplay():
 def register():
     clear()
     name = nameEntry()
-    cpe = input("Insira o CPE (encontra-se na fatura e começa com PT): ")
+    cpe = input(fun.getString("cpeinput")) #cpeinput
+    clear()
     nif = nifEntry()
     typeOfMeter = int(typeEntry()) 
-    choice = input("""
-                    Deseja inserir uma leitura inicial? (Esta será adicionada ao sistema mas não comunicada) (SIM/NAO):  """).strip().lower()
     fun.createNewPowerPlace(name, cpe, nif, typeOfMeter)
-    if choice == "sim":
+    if initialReadingEntry():
         fun.choosePowerSpot(name)
         addReading(False)
         fun.resetChoosenPlace()
@@ -140,13 +135,13 @@ def register():
 
 def deletePlace():
     pick = placePicker()
-    state = True if (input("Deseja apagar o local {0}? (SIM para confirmar, outro valor para anular)".format(pick)).strip().lower() == "sim") else False
+    state = True if (input(fun.getString("deleteconfirm").format(pick)).strip().lower() == fun.getString("acceptword") ) else False #acceptword and deleteconfirm
     if state:
         fun.deletePowerSpot(pick)
-        print("Apagado! \n")
+        print(fun.getString("deleted")) #deleted
     else:
-        print("Anulado! \n")
-    sleep(1)
+        print(fun.getString("canceled")) #canceled
+    time.sleep(1)
 
 
 
@@ -157,7 +152,7 @@ def addReading(communicate):
         reading.append(int(input('{0}: '.format(field))))
     result = fun.updateReading(*reading)
     if not result:
-        print('A contagem é inferior à registada anteriormente. Tente novamente! \n')
+        print(fun.getString("lowreading")) #lowreading
         return
 
     if communicate:
@@ -166,10 +161,7 @@ def addReading(communicate):
 
 def communicateReading(reading):
     wb.open('https://www.e-redes.pt/pt-pt/podemos-ajudar/comunicar-leituras', new=1, autoraise=True)
-    input("""
-        Coloque a janela do browser em foco (Pressione uma zona cinzenta por cima do campo CPE) 
-        e depois volte a esta janela e pressione Enter!, quando aparecer os campos para 
-        colocar a contagem volte aqui""")        
+    input(fun.getString("browser1"))     #browser1    
     altTab()
     press('tab')
     typeT(fun.getChoosenPlaceCPE())
@@ -181,8 +173,8 @@ def communicateReading(reading):
     press('tab')
     press('enter')
     clear()
-    input('Pressione Enter para escrever a contagem! Confirme o valor e pressione "Submeter Leitura"')
-    sleep(0.2)
+    input(fun.getString("submitreading"))
+    time.sleep(0.2)
     altTab()
     press('tab')
     typeT(str(reading[0]))
@@ -195,9 +187,6 @@ def communicateReading(reading):
     press('tab')
 
 
-    
-
-
 
 
 def showLastReading():
@@ -205,50 +194,45 @@ def showLastReading():
     print('Data: {0} \n'.format(reading.pop("data")))
     for x, y in reading.items():
         print('{0}: {1}'.format(x,str(y)))
+    print("\n")
 
 
 def nameEntry():
-    choice = input("Insira um nome único para o local de consumo: ").strip()
+    choice = input(fun.getString("placename")).strip() #placename
     if fun.checkIfPlaceDoesNotExist(choice):
         return choice
     else:
-        print("Por Favor insira um nome não repetido")
+        print(fun.getString("uniquenamewarning")) #uniquenamewarning
         nameEntry()
 
 def initialReadingEntry():
-    choice = input("""
-    Deseja inserir uma leitura inicial? (SIM/NAO): """).strip().lower()
-    if choice == "nao":
+    choice = input(fun.getString("firstreadingquery")).strip().lower() #firstreadingquery
+    if choice == fun.getString("cancelword"): #cancelword
         return False
-    elif choice == "sim":
+    elif choice == fun.getString("acceptword"): #acceptword
         return True
     else:
-        print("Por Favor insira SIM ou NAO \n")
+        print(fun.getString("pleaseyn")) #pleaseyn
         initialReadingEntry()
 
 
 
 def typeEntry():
     meterSet = {"1","2","3"}
-    typeOfMeter = input("""
-                1: Contador Simples
-                2: Contador bi-horário
-                3: Contador tri-horário
-                            
-                Please enter your choice: """).strip()
+    typeOfMeter = input(fun.getString("metertype") + fun.getString("enterchoice")).strip() #metertype and selectavailable
     if typeOfMeter in meterSet:
         return typeOfMeter
     else:
-        print("Deve inserir um tipo de contador válido (1, 2 ou 3)! \n")
+        print(fun.getString("validmeter")) #validmeter
         typeEntry()
 
 
 
 
 def nifEntry():
-    nif = input("Insira o NIF assiciado ao contrato: ").strip()
+    nif = input(fun.getString("nifinput")).strip() #nifinput
     if len(nif) != 9:
-        print("O NIF deve conter 9 dígitos! \n")
+        print(fun.getString("nif9digits")) #nif9digits
         return nifEntry()
     else:
         return nif
@@ -274,32 +258,11 @@ def clear():
 
 def altTab():
     keyDown('alt')
-    sleep(.2)
+    time.sleep(.2)
     press('tab')
-    sleep(.2)
+    time.sleep(.2)
     keyUp('alt')
-    sleep(.2)
-
-""" 
-
-
-pyautogui.confirm('Is the browser window in focus?', buttons=['Yes', 'No'])
-
-# pyautogui.confirm('Enter option.', buttons=['A', 'B', 'C'])
-
-pyautogui.write('PT0002000081906841LA')
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.write('197102670')
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('enter')
- """
-
-
-
+    time.sleep(.2)
 
 
 start()
-
--- defineFunctions.py
+++ defineFunctions.py
@@ -1,26 +1,59 @@
-import pickle
+import pickle, json
 from powerSpot import powerSpot
 
-FILENAME = 'places.pw' 
+FILENAME = 'power.info' 
 
 consumptionPlaces = {}
 choosenPlace = None
 
+defaultLanguage = "en"
+choosenLanguage = None
+
+lang = None
+
+
+def initLang():
+    global choosenLanguage
+    if not choosenLanguage:
+        choosenLanguage = defaultLanguage
+    
+
+def toggleLanguage():
+    global choosenLanguage
+    if choosenLanguage == "en":
+        choosenLanguage = "pt"
+    elif choosenLanguage == "pt":
+        choosenLanguage = "en"
+
+
+def getString(id):
+    return lang[choosenLanguage][id]
 
 def save():
+    global choosenLanguage
+
+    saveFile = {"1": consumptionPlaces,
+                    "0": choosenLanguage}    
     fd = open(FILENAME, 'wb')
-    pickle.dump(consumptionPlaces, fd)
+    pickle.dump(saveFile, fd)
     fd.close()
     return True
 
 
 def load():
+    global lang
+    with open('languages.json') as json_file:
+        lang = json.load(json_file)
     try:
-        global consumptionPlaces
+        global consumptionPlaces, choosenLanguage
         fd = open(FILENAME, 'rb')
-        consumptionPlaces = pickle.load(fd)
+        saveFile = pickle.load(fd)
+        choosenLanguage = saveFile["0"]
+        consumptionPlaces = saveFile["1"]
+        #consumptionPlaces = pickle.load(fd)
         fd.close()
     except OSError:
+        initLang()
         print('No consumption places file found!')
 
 
@@ -66,7 +99,7 @@ def choosePowerSpot(name):
 
 
 def getAllPowerSpotsName():
-    lista = list(consumptionPlaces.keys()) #THIS NEEDS TO BE DONE CAUSE THE LIST IS SORTED IN PLACE DOESNT RETURN SORTED LIST
+    lista = list(consumptionPlaces.keys()) 
     lista.sort()
     return lista
 
